5c42dd228b6d7a9997662935182e296419d79cc3
Dallas Sherman <appledododo@Dallass-MacBook-Air.local>
2024-11-28 20:38:41 +0900

    naisu

--- control_flow/src/main.rs
+++ control_flow/src/main.rs
@@ -16,6 +16,7 @@ fn main() {
     } else {
         println!("wowza, this is lower than 3");
     };
+
     // can also add if's into a let.
 
     let condition = true;
@@ -26,7 +27,7 @@ fn main() {
 
     let mut counter = 0;
 
-    let result = 'counting_up: loop {
+    let result = loop {
         counter += 1;
 
         if counter == 10 {
@@ -35,7 +36,7 @@ fn main() {
     };
 
     println!("The result is {result}");
-
+    // labelling loops to disambiguate between multiple loops.
     let mut count = 0;
     'counting_up: loop {
         println!("count = {count}");
@@ -54,5 +55,43 @@ fn main() {
     }
     println!("End count = {count}");
 
+    //infinite loops
+
+    // loop {
+    //    println!("AGAAAAAINNN");
+    // }
+
     // while loops
+
+    let mut number_thrice = 3;
+
+    while number_thrice != 0 {
+        println!("{number_thrice}");
+
+        number_thrice -= 1;
+    }
+    println!("LIFTOFF!!");
+
+    // loop with for (Slow Compile time)
+    let ah = [10, 20, 30, 40, 50];
+    let mut index = 0;
+
+    while index < 5 {
+        println!("the value is: {}", ah[index]);
+
+        index += 1;
+    }
+    // OR to be more concise I can.... (way safer from array resizing and speedier in run time)
+
+    let ahh = [10, 20, 30, 40, 50];
+
+    for element in ahh {
+        println!("the value is: {element}");
+    }
+    // how about a litle reving booooiiiisss
+
+    for number in (1..4).rev() {
+        println!("{number}!");
+    }
+    println!("LIFTOFF....er....again!");
 }
