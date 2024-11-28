c5e886bb7ce51d62a1c01e14886f96fd9f955b32
Dallas Sherman <appledododo@Dallass-MacBook-Air.local>
2024-11-28 20:03:02 +0900

    let's GOOOOOO

--- control_flow/src/main.rs
+++ control_flow/src/main.rs
@@ -1,3 +1,58 @@
 fn main() {
-    println!("Hello, world!");
+    // if conditions
+
+    let number = 3;
+
+    if number < 5 {
+        println!("condition was true");
+    } else {
+        println!("condition was false");
+    };
+
+    if number >= 3 {
+        println!("hey this was higher than {number}");
+    } else if number == 3 {
+        println!("hey its 3, good job");
+    } else {
+        println!("wowza, this is lower than 3");
+    };
+    // can also add if's into a let.
+
+    let condition = true;
+    let number_again = if condition { 5 } else { 6 };
+    println!("The value of number is: {number_again}");
+
+    // loops
+
+    let mut counter = 0;
+
+    let result = 'counting_up: loop {
+        counter += 1;
+
+        if counter == 10 {
+            break counter * 2;
+        }
+    };
+
+    println!("The result is {result}");
+
+    let mut count = 0;
+    'counting_up: loop {
+        println!("count = {count}");
+        let mut remaining = 10;
+
+        loop {
+            if remaining == 9 {
+                break;
+            }
+            if count == 2 {
+                break 'counting_up;
+            }
+            remaining -= 1;
+        }
+        count += 1;
+    }
+    println!("End count = {count}");
+
+    // while loops
 }
---
+++ control_flow/target/debug/control_flow.d
@@ -0,0 +1,1 @@
+/Users/appledododo/Documents/rust-for-fun/control_flow/target/debug/control_flow: /Users/appledododo/Documents/rust-for-fun/control_flow/src/main.rs
