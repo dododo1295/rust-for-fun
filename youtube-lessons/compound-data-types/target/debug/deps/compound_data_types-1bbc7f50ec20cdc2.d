f5da3d093229c9dbdd6d75956785028752659108
Dallas Sherman <appledododo@Dallass-MacBook-Air.local>
2024-11-30 19:05:46 +0900

    idk what i'm pushing

--- youtube-lessons/compound-data-types/target/debug/deps/compound_data_types-1bbc7f50ec20cdc2.d
+++ youtube-lessons/compound-data-types/target/debug/deps/compound_data_types-1bbc7f50ec20cdc2.d
@@ -1,5 +1,5 @@
-/Users/appledododo/Documents/rust-for-fun/compound-data-types/target/debug/deps/libcompound_data_types-1bbc7f50ec20cdc2.rmeta: src/main.rs
+/Users/appledododo/Documents/rust-for-fun/youtube-lessons/compound-data-types/target/debug/deps/libcompound_data_types-1bbc7f50ec20cdc2.rmeta: src/main.rs
 
-/Users/appledododo/Documents/rust-for-fun/compound-data-types/target/debug/deps/compound_data_types-1bbc7f50ec20cdc2.d: src/main.rs
+/Users/appledododo/Documents/rust-for-fun/youtube-lessons/compound-data-types/target/debug/deps/compound_data_types-1bbc7f50ec20cdc2.d: src/main.rs
 
 src/main.rs:
