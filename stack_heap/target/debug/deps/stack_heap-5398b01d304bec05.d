aae9ea803380ce03c1d9f68784c0b06cd20ebd2f
Dallas S. <andrewdallassherman@gmail.com>
2024-12-23 13:53:46 +0900

    I don't know what this is lol

---
+++ stack_heap/target/debug/deps/stack_heap-5398b01d304bec05.d
@@ -0,0 +1,5 @@
+/Users/appledododo/Documents/rust-for-fun/stack_heap/target/debug/deps/libstack_heap-5398b01d304bec05.rmeta: src/main.rs
+
+/Users/appledododo/Documents/rust-for-fun/stack_heap/target/debug/deps/stack_heap-5398b01d304bec05.d: src/main.rs
+
+src/main.rs:
---
+++ stack_heap/target/debug/deps/stack_heap-d7ced3fc4488313e.d
@@ -0,0 +1,5 @@
+/Users/appledododo/Documents/rust-for-fun/stack_heap/target/debug/deps/libstack_heap-d7ced3fc4488313e.rmeta: src/main.rs
+
+/Users/appledododo/Documents/rust-for-fun/stack_heap/target/debug/deps/stack_heap-d7ced3fc4488313e.d: src/main.rs
+
+src/main.rs:
