6dc0c959c49bcf03d29bcba7a8d7c53c07a80633
Dallas S. <andrewdallassherman@gmail.com>
2025-01-02 10:05:04 +0900

    don't know what these are.. just pushing to clear

---
+++ youtube-lessons/compound-data-types/target/debug/deps/compound_data_types-c97b15341dded734.d
@@ -0,0 +1,5 @@
+/Users/appledododo/Documents/rust-for-fun/youtube-lessons/compound-data-types/target/debug/deps/libcompound_data_types-c97b15341dded734.rmeta: src/main.rs
+
+/Users/appledododo/Documents/rust-for-fun/youtube-lessons/compound-data-types/target/debug/deps/compound_data_types-c97b15341dded734.d: src/main.rs
+
+src/main.rs:
